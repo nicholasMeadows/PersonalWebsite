b7fd19cc954ed626018415e45a084e22f388f4ca
nicholasMeadows <meadowsnick98@gmail.com>
2024-04-25 16:02:09 -0500

    Add alt for imgs on nav bar

--- src/app/navigation-bar-github-img.tsx
+++ src/app/navigation-bar-github-img.tsx
@@ -1,11 +1,13 @@
 'use client';
+import Image from "next/image";
+
 export default function NavigationBarGithubImg() {
-    return <img src={'github-logo.png'} style={{
+    return <Image src={'github-logo.png'} style={{
         width: 'auto',
         height: '100%',
         marginRight: '1em',
         cursor: 'pointer'
-    }} onClick={() => {
+    }} alt={''} onClick={() => {
         window.open('https://github.com/nicholasMeadows')
     }}/>
 }
--- src/app/navigation-bar-linkedin-img.tsx
+++ src/app/navigation-bar-linkedin-img.tsx
@@ -1,11 +1,13 @@
 'use client';
+import Image from "next/image";
+
 export default function NavigationBarLinkedinImg() {
-    return <img src={'linkedin-logo.png'} style={{
+    return <Image src={'linkedin-logo.png'} style={{
         width: 'auto',
         height: '100%',
         marginRight: '1em',
         cursor: 'pointer'
-    }} onClick={() => {
+    }} alt={''} onClick={() => {
         window.open('https://www.linkedin.com/in/nicholasmeadows/')
     }}/>
 }
