56590fe836e213d88a62f098b4a3855df9a72a8a
nicholasMeadows <meadowsnick98@gmail.com>
2024-04-26 00:20:24 -0500

    Fix Build Issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
     return (
         <div className={'home-page'}>
             <div className={'home-page-text'}>
-                <p>Hello, I'm Nick</p>
+                <p>Hello, I&apos;m Nick</p>
                 <br/>
-                <p className={'indent'}>Welcome to my corner of the internet! I'm a Senior Software Developer
+                <p className={'indent'}>Welcome to my corner of the internet! I&apos;m a Senior Software Developer
                     with a
                     passion for building scalable
                     and resilient web applications. With a strong background in Java, Spring Boot, HTML, CSS, and
@@ -14,7 +14,7 @@ export default function Home() {
                     strive to create innovative and efficient solutions that meet and exceed client
                     expectations.</p>
                 <br/>
-                <p className={'indent'}>In my professional journey, I've had the opportunity to work on
+                <p className={'indent'}>In my professional journey, I&apos;ve had the opportunity to work on
                     diverse
                     projects, from developing dynamic
                     websites for credit card processing companies to crafting REST services for satellite imagery
@@ -23,11 +23,12 @@ export default function Home() {
                     focus on quality assurance ensures that every project I undertake is delivered to the highest
                     standards.</p>
                 <br/>
-                <p className={'indent'}>Whether you're here to learn more about my professional experience or
+                <p className={'indent'}>Whether you&apos;re here to learn more about my professional experience or
                     simply
                     share a passion for
                     technology
-                    and creativity, I'm thrilled to have you visit. Feel free to explore my website. Let's innovate
+                    and creativity, I&apos;m thrilled to have you visit. Feel free to explore my website. Let&apos;s
+                    innovate
                     and
                     inspire together!</p>
             </div>
