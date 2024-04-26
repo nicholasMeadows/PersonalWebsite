180616be1809bf4c5a2de32fdb7602acf9f9cade
nicholasMeadows <meadowsnick98@gmail.com>
2024-04-26 14:02:22 -0500

    Add to work experience page

--- src/app/work-experience/page.tsx
+++ src/app/work-experience/page.tsx
@@ -217,6 +217,78 @@ export default function WorkExperience() {
                     </ul>
                 </div>
             </div>
+            <h1>Other Experience</h1>
+            <div className={'other-experience-box'}>
+                <h3>Nano Racks</h3>
+                <p>During my high school years, I had the remarkable opportunity to lead a team in coordinating a
+                    groundbreaking science experiment sponsored by Nanoracks, Valley Christian High School, and Quest
+                    for Excellence. Our project aimed to investigate the effectiveness of antibiotics on E. Coli
+                    bacteria in the unique microgravity environment of the International Space Station (ISS).</p>
+                <br/>
+                <div className={'other-experience-img-box'}>
+                    <img className={'other-experience-img'} src={'/NicholasSoldering.png'} alt={''}/>
+                    <img className={'other-experience-img'} src={'/NanoRacksExperiment.png'} alt={''}/>
+                </div>
+
+                <br/>
+                <p>As part of this project, I took on the role of developing a sophisticated program using Basic Stamp.
+                    This program was essential for controlling various aspects of the experiment, including capturing
+                    images, activating fluorescent LEDs, and managing pumps and vibration motors. These tasks were
+                    crucial for the successful execution of our experiment and required meticulous attention to detail
+                    and programming skills.</p>
+                <br/>
+                <p>Throughout the project, I collaborated closely with my team members, teachers, and sponsors to ensure
+                    that all aspects of the experiment were carefully planned and executed. This included designing and
+                    building the experiment payload, which had to fit within the size constraints of a stick of butter
+                    to be compatible with the ISS.</p>
+                <br/>
+                <p>Overall, the Nanoracks project was a transformative experience that not only deepened my passion for
+                    science and technology but also equipped me with valuable skills and knowledge that I continue to
+                    apply in my academic and professional pursuits.</p>
+
+            </div>
+            <div className={'other-experience-box'}>
+                <h3>First Robotics Team 4353</h3>
+                <br/>
+                <img className={'other-experience-img other-experience-centered-img-box-50-percent-width'}
+                     src={'/FRC Team 4353 2016 Robot.png'} alt={''}/>
+                <br/>
+                <p>As a member of FRC Team 4353, also known as Krewe de Reauxbotics, my four-year journey through high
+                    school was transformed into an unforgettable experience filled with challenges, triumphs, and
+                    invaluable lessons. From planning and designing to building and competing, my time on the robotics
+                    team was a culmination of teamwork, innovation, and passion for STEM.</p>
+                <br/>
+                <p>One of the most rewarding aspects of my involvement with the team was the opportunity to work on all
+                    facets of robotics. I played a pivotal role in planning our robot`&apos;s design, meticulously
+                    crafting
+                    components, and refining our strategies for competitions. Whether it was brainstorming ideas for new
+                    features or troubleshooting issues during build season, every moment was a chance to learn and
+                    grow.</p>
+                <br/>
+                <p>One of my primary responsibilities on the team was programming the robot and serving as a driver
+                    during competitions. This role allowed me to showcase my technical skills and creativity, as I
+                    developed autonomous routines and control systems that optimized our robot`&apos;s performance. As a
+                    driver, I thrived under pressure, maneuvering the robot with precision and agility to complete tasks
+                    and challenges on the field.</p>
+                <br/>
+                <p>Beyond the technical aspects, being part of FRC Team 4353 taught me the importance of teamwork and
+                    collaboration. I worked closely with my teammates, mentors, and coaches, each bringing their unique
+                    skills and perspectives to the table. Together, we overcame obstacles, celebrated successes, and
+                    formed lifelong friendships.</p>
+                <br/>
+                <p>My time on the robotics team was not just about building robots; it was about building character,
+                    resilience, and a passion for innovation. The skills and lessons I learned have had a lasting impact
+                    on my life, shaping me into the person I am today. As I look back on my journey with FRC Team 4353,
+                    I am filled with gratitude for the experiences, friendships, and memories that will stay with me
+                    forever.</p>
+                <br/>
+                <p>In conclusion, my time on FRC Team 4353 was an enriching and transformative experience that taught me
+                    the value of teamwork, perseverance, and passion. It was more than just a robotics team; it was a
+                    family united by a shared love for technology and a commitment to excellence. I am proud to have
+                    been a part of such an incredible journey and look forward to applying the skills and lessons
+                    I`&apos;ve
+                    learned to future endeavors.</p>
+            </div>
         </div>
     </div>)
 }
