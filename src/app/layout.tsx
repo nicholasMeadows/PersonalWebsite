95b96a9f996e6617674ee12691b7890052b5106b
nicholasMeadows <meadowsnick98@gmail.com>
2024-04-30 16:27:10 -0500

    Remove and reset gtm and ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./css/globals.css";
 import NavigationBar from "@/app/components/navigation-bar";
 import {NAVIGATION_BAR_HEIGHT} from "@/app/constants/navigation-bar";
 import ThemeProvider from "@/app/components/ThemeProvider";
-import Script from "next/script";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -20,27 +19,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <head>
-            <Script>
-                {
-                    '<!-- Google Tag Manager -->\n' +
-                    '                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':\n' +
-                    '                    new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],\n' +
-                    '                    j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=\n' +
-                    '                    \'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);\n' +
-                    '                })(window,document,\'script\',\'dataLayer\',\'GTM-KFTG4XHK\');'
-                }
-
-            </Script>
-        </head>
         <body className={inter.className}>
-
-        <noscript>
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KFTG4XHK"
-                    height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe>
-        </noscript>
-
-        {/*<GoogleTagManager gtmId='GTM-KFTG4XHK'/>*/}
         <ThemeProvider>
             <div style={{height: '100vh'}}>
                 <NavigationBar/>
@@ -52,7 +31,6 @@ export default function RootLayout({
                 </div>
             </div>
         </ThemeProvider>
-        {/*<GoogleAnalytics gaId="G-GDJW8VGVTY"/>*/}
         </body>
         </html>
     );
