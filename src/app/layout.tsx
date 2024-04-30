8a5af3866e8fd8c32d91434f59f2eebc8ffa58cb
nicholasMeadows <meadowsnick98@gmail.com>
2024-04-30 17:45:46 -0500

    Add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,17 +23,30 @@ export default function RootLayout({
         <body className={inter.className}>
 
 
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-YTLWWK4PC9"></Script>
-        <Script id={'google-analytics'}>
+        {/*<Script async src="https://www.googletagmanager.com/gtag/js?id=G-YTLWWK4PC9"></Script>*/}
+        {/*<Script id={'google-analytics'}>*/}
+        {/*    {`*/}
+        {/*    window.dataLayer = window.dataLayer || [];*/}
+        {/*    function gtag(){dataLayer.push(arguments);}*/}
+        {/*    gtag('js', new Date());*/}
+
+        {/*    gtag('config', 'G-YTLWWK4PC9');*/}
+        {/*    `}*/}
+        {/*</Script>*/}
+
+
+        <Script id={'google-tag-manager'}>
             {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-YTLWWK4PC9');
-            `}
-        </Script>
-
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TRX4R5R8');
+`}</Script>
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRX4R5R8"
+                    height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe>
+        </noscript>
 
         <ThemeProvider>
             <div style={{height: '100vh'}}>
